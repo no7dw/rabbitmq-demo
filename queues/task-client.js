8bc64ef6b16f0fe1252765582c881f0ea4a3ef0a
no7dw <no7david@gmail.com>
2016-09-22 19:35:47 +0800

    send twice

--- queues/task-client.js
+++ queues/task-client.js
@@ -9,6 +9,8 @@ amqp.connect('amqp://localhost', function(err, conn) {
 
     ch.assertQueue(q, {durable: true});//msg won't lost event if rabbitMQ restarts
     ch.sendToQueue(q, new Buffer(msg), {persistent: true});//msg won't lost event if rabbitMQ restarts
+    //try send twice
+    ch.sendToQueue(q, new Buffer(msg), {persistent: true});
     console.log(" [x] Sent '%s'", msg);
   });
   setTimeout(function() { conn.close(); process.exit(0) }, 10); //don't set timeout time 0 -- you know why... exit before send msg to rabbitmq
