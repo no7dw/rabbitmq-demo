ece409ee09127d929c5a2e1bc3246650e14bafcf
no7dw <no7david@gmail.com>
2018-05-16 20:50:46 +0800

    use redis counter to verify prefetch

--- rpc/rpc_server.js
+++ rpc/rpc_server.js
@@ -4,22 +4,44 @@ var amqp = require('amqplib/callback_api');
 var config = require('./config.dev')
 var currentLevel = 0
 var bluebird = require('bluebird')
-
+var Redis = require('ioredis');
+var redis = new Redis();
+function initRedis(){
+  redis.set('rpccount', 0 )
+}
+function decRediscount(){
+  redis.decr('rpccount')
+}
+function incRediscount(){
+  redis.incr('rpccount')
+}
 async function setupServer(queue, handler){
+  initRedis()
   amqp.connect(config.RABBITMQ_URL, function(err, conn) {  
+    if(err){
+      console.log("err", err);
+      return
+    }
     conn.createChannel(function(err, ch) {
       ch.assertQueue(queue, {durable: false});
-      ch.prefetch(1);
+      ch.prefetch(3);
       console.log(' [x] Awaiting RPC requests');
       ch.consume(queue, function(req) {
+        incRediscount()
+        // console.log("count",count)
         handler(req)
         .then(fResult =>{
           console.log("handle complete, send result to client", fResult);
           ch.sendToQueue(req.properties.replyTo,new Buffer( JSON.stringify( fResult) ), {correlationId: req.properties.correlationId});//sync send the result back to client
-          console.log('delay finish in 5s');
-          bluebird.delay(5000).then(function(){
+          // ch.ack(req);
+          var rdn = parseInt(Math.random()*1000*5);
+          console.log('delay finish in %d ms' , rdn);
+          bluebird.delay(rdn).then(function(){
+            decRediscount()
             ch.ack(req);  
             console.log('finish ack');
+            ch.get(queue);
+            ch.cancel();
           });
         });
       });
